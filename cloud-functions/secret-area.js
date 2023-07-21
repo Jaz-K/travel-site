7012a76af6cee4f05988becad834530e968965a3
Jasmin Köhler <jasminangelique@gmail.com>
2023-07-21 13:28:04 +0200

    addet client area feature

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function (event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area",
-    });
+    const secretContent = `
+    <h3>Welcome to the Secret Area</h3>
+    <p>Here we can tell you the sky is <strong>blue</strong> and two plus two is equals to four </p>
+    `;
+
+    let body;
+    let password = "javascript";
+
+    if (event.body) {
+        body = JSON.parse(event.body);
+    } else {
+        body = {};
+    }
+
+    if (body.password == password) {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent,
+        });
+    } else {
+        callback(null, {
+            statusCode: 401,
+        });
+    }
 };
